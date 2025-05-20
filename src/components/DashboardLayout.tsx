49406bb74a101020bec02c7d11676bcba4c769d8
Gum Priscilla <priscillasky542@gmail.com>
2025-05-20 15:28:12 +0300

    modified layout

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -3,6 +3,7 @@ import { Header } from "@/components/common/Header";
 import { Sidebar } from "@/components/common/Sidebar";
 import { useSidebar } from "@/components/SidebarProvider";
 import { ReactNode } from "react";
+import Head from "next/head";
 
 export default function DashboardLayout({
   children,
@@ -12,6 +13,11 @@ export default function DashboardLayout({
   const { collapsed } = useSidebar();
 
   return (
+    <>
+    <Head>
+        <title>Dashboard</title>
+      </Head>
+  
     <div className="relative flex min-h-screen">
       <div className="hidden md:block">
         <Sidebar />
@@ -25,5 +31,6 @@ export default function DashboardLayout({
         <main className="flex-1">{children}</main>
       </div>
     </div>
+    </>
   );
 }
